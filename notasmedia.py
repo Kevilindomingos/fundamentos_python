f5e9cf907bc5bf6df68d137d7a93b15c4c712b9d
Kevilin Marcondes <kevilinmarcondes356@gmail.com>
2026-03-30 21:18:19 -0300

    Add files via upload

---
+++ notasmedia.py
@@ -0,0 +1,18 @@
+# Solicita as notas do usuário
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+# Calcula a média das notas
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Verifica se o estudante foi aprovado
+if media >= 70:
+    print("O estudante foi aprovado")
+else:
+    print("O estudante foi reprovado")
+
+# Exibe a média
+    print(f"A média do estudante é: {media:.2f}")
